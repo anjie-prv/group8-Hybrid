b0e2acb58915417c09183ba6a5c1af0978713d86
Parvatie Persaud <persapar@sheridancollege.ca>
2025-04-04 23:48:36 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'group8TaskApp',
+  webDir: 'www'
+};
+
+export default config;
