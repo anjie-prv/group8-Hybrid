38b0f304954122d05145745d9ff76e6d0c1fcec9
Parvatie Persaud <persapar@sheridancollege.ca>
2025-04-04 23:53:42 -0400

    for the base . missing gr8server.js for the mongoosenode server in separate folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'school',
+    loadChildren: () => import('./school/school.module').then( m => m.SchoolPageModule)
+  },
+  {
+    path: 'work',
+    loadChildren: () => import('./work/work.module').then( m => m.WorkPageModule)
+  },
+  {
+    path: 'social',
+    loadChildren: () => import('./social/social.module').then( m => m.SocialPageModule)
   }
 ];
 @NgModule({
