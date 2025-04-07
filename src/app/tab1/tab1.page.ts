9b5d9536b39a5758c867053d66c0e9082d21ae9b
Parvatie Persaud <persapar@sheridancollege.ca>
2025-04-06 23:20:33 -0400

    tab1 works the node server showed that the info was collected and stored!

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // this is where the input of tasks will be  = a form like in  inclass wk11
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskAppService } from '../task-app.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: false,
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  taskForm!: FormGroup;
 
+
+  constructor(private fb: FormBuilder, private taskService: TaskAppService) {}
+    
+  
+ngOnInit() {
+  this.taskForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    category: ['school', Validators.required]
+  });
 }
+
+submitTask() {
+  if (this.taskForm.valid) {
+    const newTask = {
+      title: this.taskForm.value.title,
+      description: this.taskForm.value.description,
+      category: this.taskForm.value.category,
+      completed: false
+    };
+
+      console.log('Submitting task:', newTask);
+
+
+    this.taskService.createTask(newTask).subscribe(
+      response => {
+        console.log('Task added:', response);
+        this.taskForm.reset({ category: 'school' }); // reset form with default category
+      },
+      error => {
+        console.error('Error adding task', error);
+      }
+    );
+  } else {
+    console.warn('Form is invalid');
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     ngOnInit() {
+//     this.taskForm = this.fb.group({
+//       title: ['', Validators.required],
+//       description: [''],
+//       category: ['', Validators.required],
+//     });
+//     }
+
+//   submitTask(){
+//     if (this.taskForm.valid) {
+//       const newTask = {
+//         title: this.taskForm.get('title')?.value,
+//         description: this.taskForm.get('description')?.value,
+//         category: this.taskForm.get('category')?.value,
+//         completed: false
+//       };
+
+//       this.taskService.createTask(newTask).subscribe(
+//         response => {
+//           console.log('Task added:', response);
+//           this.taskForm.reset(); // reset form after submit
+//         },
+//         error => {
+//           console.error('Error adding task', error);
+//         }
+//       );
+//     }
+//   }
+// }
+
--- src/app/task-app.service.ts
+++ src/app/task-app.service.ts
@@ -1,7 +1,7 @@
 //handles all the HTTP requests as shown in the wk11 inclass
 import { Injectable } from '@angular/core';
 // import httpclient
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -21,15 +21,26 @@ export class TaskAppService {
   }
 
   getTasks(filter?: any): Observable<any>{
+    let params = new HttpParams();
+    if (filter) {
+      Object.keys(filter).forEach(key => {
+        params = params.set(key, filter[key]);
+      });
+    }
     return this.http.post(`${this.apiUrl}/allTasks`, {params: filter});
   }
 
   getTasksByCategory(category: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?category=${category}`);
+    let url = `${this.apiUrl}/allTasks`;
+    if (category) {
+      url += `?category=${category}`;
+    }
+    return this.http.get<any[]>(url);
   }
 
+
   updateTask(taskId: string, updates:any): Observable<any>{
-    return this.http.put(`${this.apiUrl}/${taskId}`, updates);
+    return this.http.put(`${this.apiUrl}/tasks/${taskId}`, updates);
   }
 
 }
